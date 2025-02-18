b1758416f997014ccbabf1363822cb466825fb22
Meredith Lancaster <malancas@github.com>
2025-02-18 16:02:34 -0700

    add unit test that mirrors the integration test
    Signed-off-by: Meredith Lancaster <malancas@github.com>

--- pkg/cmd/attestation/verification/sigstore_test.go
+++ pkg/cmd/attestation/verification/sigstore_test.go
@@ -3,6 +3,7 @@ package verification
 import (
 	"testing"
 
+	"github.com/cli/cli/v2/pkg/cmd/attestation/api"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -21,15 +22,16 @@ func TestSigstoreVerifier(t *testing.T) {
 		assert.Len(t, results, 2)
 	})
 
-	t.Run("with 1/2 verified attestations - sigstore verification failed", func(t *testing.T) {
-		verifier := newVerifierWithFailAfterNCallsVerifier(1)
+	t.Run("with 2/3 verified attestations - sigstore verification failed", func(t *testing.T) {
+		verifier := newVerifierWithFailAfterNCallsVerifier(2)
 
-		attestations := GetAttestationsFor(t, "../test/data/sigstore-js-2.1.0_with_2_bundles.jsonl")
-		require.Len(t, attestations, 2)
+		bundles := GetAttestationsFor(t, "../test/data/sigstore-js-2.1.0_with_2_bundles.jsonl")
+		attestations := []*api.Attestation{bundles[0], bundles[1], bundles[0]}
+		require.Len(t, attestations, 3)
 
 		results, err := verifier.Verify(attestations, PublicGoodPolicy(t))
 		assert.NoError(t, err)
-		assert.Len(t, results, 1)
+		assert.Len(t, results, 2)
 	})
 
 	t.Run("fail with 0/2 verified attestations", func(t *testing.T) {
