0e9eef6f51902dee8e84786de2b513e782e8bd05
Meredith Lancaster <malancas@github.com>
2025-02-18 18:38:24 -0700

    only use require
    Signed-off-by: Meredith Lancaster <malancas@github.com>

--- pkg/cmd/attestation/verification/sigstore_test.go
+++ pkg/cmd/attestation/verification/sigstore_test.go
@@ -4,7 +4,6 @@ import (
 	"testing"
 
 	"github.com/cli/cli/v2/pkg/cmd/attestation/api"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -18,8 +17,8 @@ func TestSigstoreVerifier(t *testing.T) {
 		require.Len(t, attestations, 3)
 
 		results, err := verifier.Verify(attestations, PublicGoodPolicy(t))
-		assert.NoError(t, err)
-		assert.Len(t, results, 2)
+		require.NoError(t, err)
+		require.Len(t, results, 2)
 	})
 
 	t.Run("with 2/3 verified attestations - sigstore verification failed", func(t *testing.T) {
@@ -30,8 +29,8 @@ func TestSigstoreVerifier(t *testing.T) {
 		require.Len(t, attestations, 3)
 
 		results, err := verifier.Verify(attestations, PublicGoodPolicy(t))
-		assert.NoError(t, err)
-		assert.Len(t, results, 2)
+		require.NoError(t, err)
+		require.Len(t, results, 2)
 	})
 
 	t.Run("fail with 0/2 verified attestations", func(t *testing.T) {
@@ -41,7 +40,7 @@ func TestSigstoreVerifier(t *testing.T) {
 		require.Len(t, attestations, 2)
 
 		results, err := verifier.Verify(attestations, PublicGoodPolicy(t))
-		assert.Error(t, err)
-		assert.Nil(t, results)
+		require.Error(t, err)
+		require.Nil(t, results)
 	})
 }
